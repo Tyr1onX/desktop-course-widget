a841145940a70be87dd19810aeffa75a3831e163
循迹 <yit38331@gmail.com>
2026-07-25 22:32:57 +0800

    design: replace the temporary mark with the app icon

--- website/docs/.vitepress/theme/HomePage.vue
+++ website/docs/.vitepress/theme/HomePage.vue
@@ -4,7 +4,7 @@ import { withBase } from 'vitepress'
 const guideLink = withBase('/guide/getting-started')
 const faqLink = withBase('/help/faq')
 const developmentLink = withBase('/development/')
-const markLink = withBase('/mark.svg')
+const markLink = withBase('/app-icon.png')
 const releaseLink = 'https://github.com/Tyr1onX/desktop-course-widget/releases/latest'
 const repositoryLink = 'https://github.com/Tyr1onX/desktop-course-widget'
 </script>
