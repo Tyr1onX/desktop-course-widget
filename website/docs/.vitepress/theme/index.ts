eabcd40a07978b5533ec131e9caeaa0901a055e5
循迹 <3059334571@qq.com>
2026-07-26 09:57:56 +0800

    fix: polish website timeline and detail spacing (#16)
    * fix: align website timeline and detail spacing
    
    * fix: load website layout polish styles

--- website/docs/.vitepress/theme/index.ts
+++ website/docs/.vitepress/theme/index.ts
@@ -2,6 +2,7 @@ import DefaultTheme from 'vitepress/theme'
 import Layout from './Layout.vue'
 import './custom.css'
 import './motion.css'
+import './layout-polish.css'
 
 export default {
   extends: DefaultTheme,
