5587ac4bd71a2f1b5e143c55b3d1a910437f8a61
循迹 <yit38331@gmail.com>
2026-07-26 22:25:32 +0800

    style: load orbit accuracy fixes

--- website/docs/.vitepress/theme/index.ts
+++ website/docs/.vitepress/theme/index.ts
@@ -14,6 +14,7 @@ import './settings-showcase.css'
 import './settings-showcase-positioning.css'
 import './three-part-homepage.css'
 import './orbit-homepage-fixes.css'
+import './orbit-accuracy-fixes.css'
 
 export default {
   extends: DefaultTheme,
