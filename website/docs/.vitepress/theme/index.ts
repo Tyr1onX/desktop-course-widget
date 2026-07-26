01df99e3284bb27f7367d0e9303b4b3f4001f91b
循迹 <3059334571@qq.com>
2026-07-26 13:13:15 +0800

    design: polish timeline type and desktop interaction
    * design: polish timeline typography and desktop interaction
    
    * design: load focus polish styles

--- website/docs/.vitepress/theme/index.ts
+++ website/docs/.vitepress/theme/index.ts
@@ -3,6 +3,7 @@ import Layout from './Layout.vue'
 import './custom.css'
 import './motion.css'
 import './layout-polish.css'
+import './focus-polish.css'
 
 export default {
   extends: DefaultTheme,
