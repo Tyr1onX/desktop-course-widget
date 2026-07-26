70a0c019b53c02a4fa17b2560fb77556b9d63bf2
循迹 <yit38331@gmail.com>
2026-07-27 07:36:08 +0800

    refactor: exclude development pages from app build

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,6 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        prototype: resolve(__dirname, 'index.html'),
-        gallery: resolve(__dirname, 'gallery.html'),
         widget: resolve(__dirname, 'widget.html'),
         settings: resolve(__dirname, 'settings.html'),
       },
