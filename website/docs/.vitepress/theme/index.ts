095ed18f12edf0677f4f7fca0c47e67111395ff3
循迹 <yit38331@gmail.com>
2026-07-28 00:09:55 +0800

    fix: load phone homepage layout overrides last

--- website/docs/.vitepress/theme/index.ts
+++ website/docs/.vitepress/theme/index.ts
@@ -14,6 +14,7 @@ import './orbit-homepage-fixes.css'
 import './orbit-accuracy-fixes.css'
 import './responsive-audit.css'
 import './time-motion.css'
+import './mobile-home.css'
 
 export default {
   extends: DefaultTheme,
