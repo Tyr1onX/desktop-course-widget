85686daf99bc05144297b1940b00dbc0608b6631
循迹 <yit38331@gmail.com>
2026-08-04 09:18:50 +0800

    feat(promo): register layered assembly composition

--- promo-video/src/Root.tsx
+++ promo-video/src/Root.tsx
@@ -1,4 +1,5 @@
 import {Composition} from 'remotion';
+import {LogoAssemblyProof} from './compositions/LogoAssemblyProof';
 import {LogoFormation} from './compositions/LogoFormation';
 import {RibbonMeshProof} from './compositions/RibbonMeshProof';
 import {RIBBON_PROOF} from './mesh/ribbonMeshConfig';
@@ -18,6 +19,14 @@ export const RemotionRoot = () => {
       <Composition
         id="RibbonMeshProof"
         component={RibbonMeshProof}
+        durationInFrames={RIBBON_PROOF.durationInFrames}
+        fps={RIBBON_PROOF.fps}
+        width={RIBBON_PROOF.width}
+        height={RIBBON_PROOF.height}
+      />
+      <Composition
+        id="LogoAssemblyProof"
+        component={LogoAssemblyProof}
         durationInFrames={RIBBON_PROOF.durationInFrames}
         fps={RIBBON_PROOF.fps}
         width={RIBBON_PROOF.width}
